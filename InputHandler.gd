39366d219f44f3f3f26b49840dabeffd2594e10d
Revan Ji <jiruifancr@gmail.com>
2023-08-26 18:24:21 -0500

    Add flames and orbital correction

--- InputHandler.gd
+++ InputHandler.gd
@@ -8,7 +8,12 @@ enum InputState {
 
 var input_state = InputState.Normal
 
+
+var brake_speed = 10
+var turn_factor = 0.05
+
 @export var spaceship: Spaceship
+@export var camera: Camera2D
 
 var cache_spaceship_velocity: Vector2
 
@@ -25,6 +30,16 @@ func _physics_process(delta):
 				drag_start_position = get_global_mouse_position()
 				spaceship.process_mode = Node.PROCESS_MODE_DISABLED
 				input_state = InputState.GetBigJump
+			if Input.is_action_pressed("thruster_forward"):
+				spaceship.add_thruster()
+			else:
+				spaceship.disable_flame()
+			if Input.is_action_pressed("thruster_brake"):
+				spaceship.velocity *= 0.95
+			if Input.is_action_pressed("thruster_left_turn"):
+				spaceship.turn_heading(-turn_factor)
+			if Input.is_action_pressed("thruster_right_turn"):
+				spaceship.turn_heading(turn_factor)
 		InputState.GetBigJump:
 			var mouse_velocity = (drag_start_position - get_global_mouse_position()) * 0.1
 			spaceship.set_big_jump_display(
@@ -38,7 +53,14 @@ func _physics_process(delta):
 			if Input.is_action_just_released("big_jump"):
 				if mouse_velocity.length() > 10:
 					spaceship.load_velocity(mouse_velocity)
+					spaceship.show_big_jump_flame()
 				spaceship.process_mode = Node.PROCESS_MODE_INHERIT
 				input_state = InputState.Normal
 			spaceship.velocity = mouse_velocity
 	
+func _unhandled_input(event):
+	if event is InputEventMouseButton && event.is_pressed():
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			camera.zoom /= 1.1
+		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			camera.zoom *= 1.1
--- Spaceship.gd
+++ Spaceship.gd
@@ -3,12 +3,14 @@ class_name Spaceship
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var forward_speed = 0.5
 
 @onready var visual = $SpaceshipVisual
 @onready var big_jump_display = $BigJumpDisplay
 
 var velocity_cached = Vector2.INF
 var field_dir: Vector2
+var heading_direction: Vector2
 
 func _physics_process(delta):
 	if (velocity_cached != Vector2.INF):
@@ -16,13 +18,17 @@ func _physics_process(delta):
 		velocity_cached = Vector2.INF
 		
 	velocity += field_dir * delta
-	visual.set_heading(velocity)
+	visual.set_heading(heading_direction)
 	move_and_slide()
 
+func turn_heading(rad):
+	heading_direction = heading_direction.rotated(rad)
+	
 
 # this gurantees cached velocity to be loaded by next frame start
 func load_velocity(vel: Vector2):
 	velocity_cached = vel
+	heading_direction = vel.normalized()
 
 func set_big_jump_display(strength: float, direction: Vector2):
 	big_jump_display.visible = true
@@ -31,3 +37,17 @@ func set_big_jump_display(strength: float, direction: Vector2):
 
 func hide_big_jump_display():
 	big_jump_display.visible = false
+
+func add_thruster():
+	velocity += heading_direction * forward_speed
+	$SpaceshipVisual/FlameRed.show()
+
+func disable_flame():
+	$SpaceshipVisual/FlameRed.hide()
+
+func show_big_jump_flame():
+	var tween = get_tree().create_tween()
+	tween.tween_property($SpaceshipVisual/FlameBlue, "modulate", Color.WHITE, 0.1)
+	tween.tween_property($SpaceshipVisual/FlameBlue, "modulate", Color.WHITE, 0.25)
+	tween.tween_property($SpaceshipVisual/FlameBlue, "modulate", Color.TRANSPARENT, 0.1)
+
