5341725ee35bcecd35f485711f311a65a04568ad
Revan Ji <jiruifancr@gmail.com>
2023-11-24 15:06:43 -0600

    functional chapter select menu

--- Scripts/LevelSelectPanel.gd
+++ Scripts/LevelSelectPanel.gd
@@ -1,6 +1,26 @@
+@tool
 extends Panel
 
 @export var level_path: String
+@export var level_thumbnail: Texture:
+	set(value):
+		level_thumbnail = value
+		$MarginContainer/TextureRect.texture = value
+
+@export var disabled: bool:
+	set(value):
+		disabled = value
+		$Button.disabled = value
+		if value:
+			$Label.text = "Coming Soon"
+			$MarginContainer/TextureRect.texture = placeholder_texture
+			
+@export var placeholder_texture: Texture
+
+@export var level_description: String:
+	set(value):
+		level_description = value
+		$Label.text = value
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file(level_path)
